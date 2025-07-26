ad34a2a0356994b076b8af8ef7f95b2e78fc4aed
mhtajari <mhtajari1379@gmail.com>
2025-07-26 20:34:18 +0330

    add trouble to neovim config

---
+++ neovim/.config/nvim/lua/plugins/trouble.lua
@@ -0,0 +1,12 @@
+return {
+  'folke/trouble.nvim',
+  opts = {},
+  cmd = 'Trouble',
+  keys = {
+    {
+      '<leader>x',
+      '<cmd>Trouble diagnostics toggle<cr>',
+      desc = 'Diagnostics (Trouble)',
+    },
+  },
+}
