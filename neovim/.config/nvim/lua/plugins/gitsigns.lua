87497ada2f90d40661e2efcc52f18e571d037a86
mhtajari <mhtajari1379@gmail.com>
2025-07-30 13:55:21 +0330

    configure git for neovim

--- neovim/.config/nvim/lua/plugins/gitsigns.lua
+++ neovim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,1 +1,10 @@
-return { 'lewis6991/gitsigns.nvim' }
+return {
+  'lewis6991/gitsigns.nvim',
+  config = function()
+    require('gitsigns').setup()
+    local map = vim.keymap.set
+    local opts = { silent = true, noremap = true }
+    local extend = vim.tbl_extend
+    map('n', '<leader>gp', ':Gitsigns preview_hunk<CR>', extend('force', opts, { desc = 'Git Preview' }))
+  end,
+}
