b70bdef99e4940688703ba878a0c16aa83b6a5d1
mhtajari <mhtajari1379@gmail.com>
2025-07-30 20:35:12 +0330

    moving between buffers using tab and shift tab

--- neovim/.config/nvim/lua/plugins/bufferline.lua
+++ neovim/.config/nvim/lua/plugins/bufferline.lua
@@ -3,6 +3,10 @@ return {
   version = '*',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   after = 'catppuccin',
+  keys = {
+    { '<Tab>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+    { '<S-Tab>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+  },
   config = function()
     require('bufferline').setup {
       highlights = require('catppuccin.groups.integrations.bufferline').get(),
